90fd8ce14af76f98b1ab54f4ff64a8ddb4603416
emily <emilyfpg@gmail.com>
2013-10-26 07:59:44 -0430

    Cambios de Registro y Modificacion de usario

---
+++ polls/forms.py
@@ -0,0 +1,89 @@
+from django import forms
+from polls.models import UsuarioPerfil
+from django.contrib.auth.models import User
+import datetime
+
+
+class RegistroUsuario(forms.Form):
+    usuario = forms.CharField(max_length=50, label='Usuario')
+    clave =  forms.CharField(max_length=20, widget=forms.PasswordInput,label='Clave')
+    nombre =  forms.CharField(max_length=50, label='Nombre')
+    apellido = forms.CharField(max_length=50, label='Apellido')
+    nacimiento = forms.CharField(max_length=50, label='Nacimiento')
+    direccion =  forms.CharField(max_length=50, label='Direccion')
+    twitter =  forms.CharField(max_length=50, label='Twitter')
+    facebook =  forms.CharField(max_length=50, label='Facebook')
+    correo =  forms.CharField(max_length=50, label='Correo')
+    privacidad =  forms.BooleanField(label='Privacidad')
+    foto = forms.ImageField(label='Foto', required=False)    
+    
+    def __init__(self,*args, **kwargs):
+        super(RegistroUsuario, self).__init__(*args, **kwargs)
+        self.fields['usuario'].widget.attrs = {'placeholder': 'usuario', 'class': 'form-control'}
+        self.fields['clave'].widget.attrs = {'placeholder': 'clave', 'class': 'form-control'}
+        self.fields['nombre'].widget.attrs = {'placeholder': 'nombre', 'class': 'form-control'}
+        self.fields['apellido'].widget.attrs = {'placeholder': 'apellido', 'class': 'form-control'}
+        self.fields['nacimiento'].widget.attrs = {'placeholder': 'nacimiento', 'class': 'datepicker form-control'}
+        self.fields['direccion'].widget.attrs = {'placeholder': 'direccion', 'class': 'form-control'}
+        self.fields['twitter'].widget.attrs = {'placeholder': 'twitter', 'class': 'form-control'}
+        self.fields['facebook'].widget.attrs = {'placeholder': 'facebook', 'class': 'form-control'}
+        self.fields['correo'].widget.attrs = {'placeholder': 'correo', 'class': 'form-control'}
+        #self.fields['privacidad'].widget.attrs = {'class': 'form-control'}
+        self.fields['foto'].widget.attrs = {'placeholder': 'foto', 'class': 'form-control'}
+   
+    def clean_nacimiento(self):
+        fecha = self.cleaned_data['nacimiento'].split('/')
+        fecha = map(int, fecha)
+        fecha = datetime.date(year=fecha[2], month=fecha[0], day=fecha[1])
+        hoy = datetime.date.today()
+        if fecha >= hoy:
+            raise forms.ValidationError("Fecha de nacimiento, no puede ser mayor o igual a la de hoy")
+        return fecha
+
+    def clean_twitter(self):
+        twitter = self.cleaned_data['twitter']
+        if twitter.startswith('@'):
+            return twitter
+        raise forms.ValidationError("El campo no comienza con un @")
+    
+    def procesar_registro(self):
+        username = self.cleaned_data['usuario']
+        password = self.cleaned_data['clave']
+        name = self.cleaned_data['nombre']
+        lastname = self.cleaned_data['apellido']
+        fechaNac = self.cleaned_data['nacimiento']
+        direccion = self.cleaned_data['direccion']
+        twitter = self.cleaned_data['twitter']
+        facebook = self.cleaned_data['facebook']
+        email = self.cleaned_data['correo']
+        privacidad = self.cleaned_data['privacidad']
+        foto = self.cleaned_data.get('foto', None)
+        usuario = User(username=username, first_name=name, last_name=lastname, email=email)
+        usuario.set_password(password)
+        usuario.save()
+        perfil = UsuarioPerfil(fechanacimiento=fechaNac, direccion=direccion, twitter=twitter, facebook=facebook, privacidad=privacidad, fkusuario=usuario)
+        if foto:
+            perfil.foto = foto
+        perfil.save()
+
+        
+class EditarUsuario(RegistroUsuario):
+    def __init__(self, *args, **kwargs):
+        super(EditarUsuario, self).__init__(*args, **kwargs)
+        self.fields['clave'].required = False
+        
+    def modificar_registro(self, usuario):
+        usuario.set_password(self.cleaned_data['clave'])
+        usuario.first_name = self.cleaned_data['nombre']
+        usuario.last_name = self.cleaned_data['apellido']
+        usuario.usuarioperfil.fechanacimiento = self.cleaned_data['nacimiento']
+        usuario.usuarioperfil.direccion = self.cleaned_data['direccion']
+        usuario.usuarioperfil.twitter = self.cleaned_data['twitter']
+        usuario.usuarioperfil.facebook = self.cleaned_data['facebook']
+        usuario.email = self.cleaned_data['correo']
+        usuario.usuarioperfil.privacidad = self.cleaned_data['privacidad']
+        foto = self.cleaned_data.get('foto', None)
+        if foto:
+            usuario.usuarioperfil.foto = foto
+        usuario.usuarioperfil.save()
+        usuario.save()
--- polls/models.py
+++ polls/models.py
@@ -1,105 +1,106 @@
 #encoding:utf-8
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 import datetime
 
 # Create your models here.
-class usuarioPerfil(models.Model):
+class UsuarioPerfil(models.Model):
 	fkusuario = models.OneToOneField(User)
-	fechanacimiento	= models.CharField(max_length=50)
+	fechanacimiento	= models.DateField(max_length=50)
 	direccion = models.CharField(max_length=100)
 	twitter = models.CharField(max_length=50)
 	facebook = models.CharField(max_length=50)
-	privacidad = models.CharField(max_length=50)
+	privacidad = models.BooleanField(max_length=50)
 	foto = models.ImageField(upload_to='imagenusuario', null=True,blank=True)
 
 	def __str__(self):
-		return self.usu_nombre
+		return self.fkusuario.username
 
 
-class calendario(models.Model):
+class Calendario(models.Model):
 	fecha = models.DateTimeField('date published')
 
 	def __str__(self):
 		return self.cal_fecha
 
 
-class instagram(models.Model):
+class Instagram(models.Model):
 	url = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.ins_url
 
 
-class soundcloud(models.Model):
+class SoundCloud(models.Model):
 	url = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.sou_url
 
 
-class youtube(models.Model):
+class YouTube(models.Model):
 	url = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.you_url
 
 
-class album(models.Model):
+class Album(models.Model):
 	nombre = models.CharField(max_length=100)
 	descripcion = models.CharField(max_length=200)
 	privacidad = models.BooleanField(default= True)
 	foto = models.ImageField(upload_to='imagenalbum')
-	fkusuario = models.ForeignKey(usuarioPerfil)
-	fkcalendario = models.ForeignKey(calendario)
+	fkusuario = models.ForeignKey(UsuarioPerfil)
+	fkcalendario = models.ForeignKey(Calendario)
 
 	def __str__(self):
 		return self.alb_nombre
 
 
-class comentario(models.Model):
+class Comentario(models.Model):
 	descripcion	= models.CharField(max_length=200)
-	fkemisor = models.ForeignKey(usuarioPerfil)
-	fkalbum	= models.ForeignKey(album)
-	fkinstagram	= models.ForeignKey(instagram)
-	fkyoutube = models.ForeignKey(youtube)
-	fksoundcloud = models.ForeignKey(soundcloud)
-	fkcalendario = models.ForeignKey(calendario)
+	fkemisor = models.ForeignKey(UsuarioPerfil)
+	fkalbum	= models.ForeignKey(Album)
+	fkinstagram	= models.ForeignKey(Instagram)
+	fkyoutube = models.ForeignKey(YouTube)
+	fksoundcloud = models.ForeignKey(SoundCloud)
+	fkcalendario = models.ForeignKey(Calendario)
 	
 	def __str__(self):
 		return self.com_descripcion
 
 
-class meGusta(models.Model):
+class MeGusta(models.Model):
 	like = models.CharField(max_length=200)
-	fkemisor = models.ForeignKey(usuarioPerfil)
-	fkalbum	= models.ForeignKey(album)
-	fkinstagram	= models.ForeignKey(instagram)
-	fkyoutube = models.ForeignKey(youtube)
-	fksoundcloud = models.ForeignKey(soundcloud)
-	fkcalendario = models.ForeignKey(calendario)
+	fkemisor = models.ForeignKey(UsuarioPerfil)
+	fkalbum	= models.ForeignKey(Album)
+	fkinstagram	= models.ForeignKey(Instagram)
+	fkyoutube = models.ForeignKey(YouTube)
+	fksoundcloud = models.ForeignKey(SoundCloud)
+	fkcalendario = models.ForeignKey(Calendario)
 
 	def __str__(self):
 		return self.mgu_like
 
-class notificacion(models.Model):
-	fkemisor = models.ForeignKey(usuarioPerfil)
-	fkalbum	= models.ForeignKey(album)
-	fkinstagram	= models.ForeignKey(instagram)
-	fkyoutube = models.ForeignKey(youtube)
-	fksoundcloud = models.ForeignKey(soundcloud)
-	fkcalendario = models.ForeignKey(calendario)
+class Notificacion(models.Model):
+	fkemisor = models.ForeignKey(UsuarioPerfil)
+	fkalbum	= models.ForeignKey(Album)
+	fkinstagram	= models.ForeignKey(Instagram)
+	fkyoutube = models.ForeignKey(YouTube)
+	fksoundcloud = models.ForeignKey(SoundCloud)
+	fkcalendario = models.ForeignKey(Calendario)
 	def __str__(self):
 		return self.not_id
 
-class relacionNotificacion(models.Model):
-	fknotificacion = models.ManyToManyField(notificacion)
+class RelacionNotificacion(models.Model):
+	fknotificacion = models.ManyToManyField(Notificacion)
 
 	def __str__(self):
 		return self.mgu_like
 
-class relacionComentario(models.Model):
-	fkcomentario = models.ManyToManyField(comentario)
+class RelacionComentario(models.Model):
+	fkcomentario = models.ManyToManyField(Comentario)
 
 	def __str__(self):
 		return self.mgu_like
